d9dd692200d63c73779abdd49f83b446e500b3b8
sebmorsch <basti.morsch@web.de>
2020-02-27 13:40:58 +0100

    Keyboard canvas implementation

---
+++ Assets/Scripts/KeyHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class KeyHandler : MonoBehaviour, 
+    IPointerEnterHandler, 
+    IPointerExitHandler, 
+    IPointerClickHandler
+{
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Klick");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Image img = this.gameObject.GetComponent<Image>();
+        img.color = new Color32(30, 40, 55,255);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Image img = this.gameObject.GetComponent<Image>();
+        img.color = Color.grey;
+    }
+}
